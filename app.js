a3b6c2c7fa738735ea0b7ac4fcf5257be4eb1a43
nascono <aryhuh@gmail.com>
2021-06-04 06:13:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get("/", (req,res)=>{
 });
 
 app.get("/compile_and_get_metadata", (req,res)=>{
-	var code = req.query.code;
+	var base_64_code = req.query.code;
+	var base_64_buffer = Buffer.from(base_64_code, 'base64');
+	var code = buff.toString('utf8');
 	var to_compile ='{"language":"Solidity","sources":{"sol.sol":{"content":"'+code+'"}},"settings":{"outputSelection":{"*":{"*":["*"]}}}}';
 	var output = JSON.parse(solc.compile(to_compile));
 	res.send(output.contracts['sol.sol']);
