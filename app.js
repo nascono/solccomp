2b6eaeb0bff854b7f5c9de947b2b5e68a903be88
nascono <aryhuh@gmail.com>
2021-06-04 05:28:50 +0300

    asdsad

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+let port = process.env.PORT || 3000
 
 app.get("/", (req,res)=>{
 	res.send("Hello World");
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
  console.log("Server running on port 80");
 });
