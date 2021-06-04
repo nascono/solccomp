814f28b3036f05ced85d717acef37758c19518c7
nascono <aryhuh@gmail.com>
2021-06-04 15:41:53 +0300

    asd

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require("express");
 var solc = require('solc');
 var app = express();
-var bodyParser = require('body-parser');
+var getRawBody = require('raw-body')
 let port = process.env.PORT || 3000
 
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({ extended: false})); 
+
+app.use(function (req, res, next) {
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '100mb',
+    encoding: contentType.parse(req).parameters.charset
+  }, function (err, string) {
+    if (err) return next(err)
+    req.text = string
+    next()
+  })
+})
 
 app.use("/", (req,res)=>{
 	res.send("Created by Ömer Fatih AĞIN</br> Sikke Company®™ All rights reserved");
+	console.log(req.text);
 });
 
 app.use("/compile_and_get_metadata", (req,res)=>{
@@ -16,14 +27,17 @@ app.use("/compile_and_get_metadata", (req,res)=>{
 	//var base_64_buffer = Buffer.from(base_64_code, 'base64');
 	//var code = base_64_buffer.toString('ascii');
 	//code = code.replace("\"", "\\\"");
-	var code = req.body;
+	//var code = req.body;
 	
 	var to_compile_first ='{"language":"Solidity","sources":{"sol.sol":{"content":"';
 	var to_compile_last = '"}},"settings":{"outputSelection":{"*":{"*":["*"]}}}}';
 	
 	//var output = JSON.parse(solc.compile(to_compile));
 	//res.send(JSON.stringify(output.contracts['sol.sol']));
-	res.send(req.body);
+	//res.send(req.body);
+	
+	
+	
 	
 });
 
