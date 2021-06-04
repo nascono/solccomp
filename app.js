df103560af62e7c55c14a4da3c03b78702b26784
nascono <aryhuh@gmail.com>
2021-06-04 04:41:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+
+app.get("/", (req,res)=>{
+	res.send("Hello World");
+});
+
+
 app.listen(3000, () => {
  console.log("Server running on port 80");
 });
