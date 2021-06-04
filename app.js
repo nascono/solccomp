4aecde69073673aa1ace4844fd984cf76345f446
nascono <aryhuh@gmail.com>
2021-06-04 03:13:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(80, () => {
+ console.log("Server running on port 80");
 });
