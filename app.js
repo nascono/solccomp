4786e3d2d7d8f2f0535b292f9ea360205c8cc0b8
nascono <aryhuh@gmail.com>
2021-06-04 05:37:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var app = express();
 let port = process.env.PORT || 3000
 
 app.get("/", (req,res)=>{
-	res.send("Hello World");
+	res.send("Created by Ömer Fatih AĞIN</br> Sikke Company®™ All rights reserved");
 });
+
+app.get("/compile_and_get_metadata", (req,res)=>{
+	var code = req.query.code;
+	res.send(code);
+});
+
 
 
 app.listen(port, () => {
